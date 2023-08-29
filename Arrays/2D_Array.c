382ff1dab5cbf940c03d7759560674e6afa5dd29
Nikolai Ruilova <ruilova31@gmail.com>
2023-08-29 12:21:51 +0200

    Added User Input

--- Arrays/2D_Array.c
+++ Arrays/2D_Array.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+int input(int numbers[2][3])
+{
+    printf("\n\tInput desired values\n");
+
+    for (int i = 0; i<2;i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+    printf("\tColumn Nr.%d\tRow Nr.%d:", j + 1, i + 1);
+    scanf("%i", &numbers[i][j]);
+        }
+    }
+}
+
 
 int main()
 {
@@ -8,20 +25,39 @@ int main()
                         {1, 2, 3},
                         {4, 5, 6}
     };
+    char rep = 'y';
+
+
+    while (rep == 'y')
+    {
+    printf("\tWelcome!\n\tThis program creates a little two-dimensional array\n");
+    printf("\tArray is of a 2x3 Dimension\n");
+    input(&numbers);
 
     int rows = sizeof(numbers)/sizeof(numbers[0]);
     int columns = sizeof(numbers[0])/sizeof(numbers[0][0]);
 
-    printf("rows %d\n", rows);
-    printf("columns %d\n", columns);
+    printf("\n\tResulting Array:");
+    printf("\n");
     //Using nested loops for looking at the 2D array
     for (int i = 0; i < 2; i++)
     {
+        printf("\t\t");
         for ( int j = 0; j < 3; j++)
         {
             printf("%d", numbers[i][j]);
         }
+         printf("\n");
+    }
+    printf("\n\tDo you want to try again with different values?\n");
+    printf("\t[y] = yes\n\t[n] = no\n\n\tUser Choice:");
+    
+    
+    scanf(" %c", &rep);
+    printf("\n------------------------------------------------------------------\n\n");
     }
 
+
+    printf("\nProgramm ended successfully\n");
     return 0;
 }
